1ea7a65be0d5a281a62c0e6b45a163a1bbfffebd
annsngkv <snegarkova@yandex.ru>
2021-04-08 23:05:15 +0700

    Edit codestyle

--- main.cpp
+++ main.cpp
@@ -23,9 +23,33 @@ int main(int argc, char *argv[])
     std::ofstream file_to_copy(fs::current_path().string() + FILE_TO_COPY_PATH);
 
     /* Считываем пользователей из файла persons.txt и помещаем их в стэк */
+    std::cout << "Считываем пользователей из файла persons.txt и помещаем их в стэк" << std::endl;
     PersonKeeper::Instance().readPersons(persons);
 
-    /* Записываем в файл file_to_copy.txt из стека информацию о людя */
+    /* Создаем копию нашего объекта stack, чтобы сам объект стэк не изменился */
+    Stack <Person> stack_copy = PersonKeeper::Instance().getStack();
+
+    /* Критический участок.
+    * Возможны исключительные ситуации такие как пустой стэк
+    */
+    try {
+         std::cout << "Показываем полученную из файла информацию" << std::endl;
+        for (int idx = 0; idx < stack_copy.getStackSize(); idx++) {
+            /* Получаем указатель на экземпляр класса Person из стэка */
+            Person *person = stack_copy.pop();
+            std::cout << person->getLastName() + " " + person->getFirstName() + " " + person->getSecondName() << std::endl;
+        }
+    } catch (const exc::EStackEmpty& e) { // Ловим все исключения типа EStackEmpty
+        /* Обработка исключений */
+        std::cout << e.what() << std::endl;
+    }
+
+
+    /*
+     * Записываем в файл file_to_copy.txt из стека информацию о людя дважды
+     * для того, чтобы проверить, что стэк не изменился
+    */
+    PersonKeeper::Instance().writePersons(file_to_copy);
     PersonKeeper::Instance().writePersons(file_to_copy);
 
     return a.exec();
